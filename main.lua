ddb63fc01efff243b90b8a027b5034dfda164387
Flamerst <gghdjjd1@gmail.com>
2016-11-09 19:26:59 -0500

    Added stuff

---
+++ main.lua
@@ -0,0 +1,32 @@
+function love.load()
+  ballX = 100;
+  ballY = 100;
+  
+  love.keyboard.setKeyRepeat( true )
+end
+
+function love.draw()
+  
+  love.graphics.rectangle("fill",ballX,ballY,100,100)
+end
+
+function love.update()
+
+end
+
+function moveLeft()
+
+end
+
+
+function love.keypressed(key, scancode, isrepeat)
+
+  if (key == "left")  then
+      ballX = ballX + 1
+  
+  elseif (key == "right")
+      ballX = ballX + 1
+  end
+
+
+end
