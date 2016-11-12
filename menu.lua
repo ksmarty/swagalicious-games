c4734c3cc837fdeb3b8decc08988d17a44682242
ksmarty <kyleschwartz2000@gmail.com>
2016-11-11 21:33:28 -0500

    Fix plz

--- menu.lua
+++ menu.lua
@@ -1,9 +1,12 @@
 local mainMenu={};
 
-function menu()
-    love.graphics.print("Yay!",75,75)
+function mainMenu.menu()
+    love.window.setTitle("Menu - Autism Checkers")
+    io.write('Would you like to play checkers(1) or exit(2)?')
+    local choice = io.read()
+    if (choice == 1) then
+      --Continue
+    elseif (choice == 2) then
+      love.window.close()
+    end
 end
-
-mainMenu.menu = menu
-
-return mainMenu
